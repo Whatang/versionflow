ccabd92c785e24e5e508febf6d8eeaa565a2752a
mike_vaio <mike@whatang.org>
2019-03-02 20:13:37 +0000

    Add a testing framework.

---
+++ test_versionflow.py
@@ -0,0 +1,60 @@
+import unittest
+
+
+class TestVersion(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testFromBumpversion(self):
+        pass
+
+
+class TestBumpVersionWrapper(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testFromExisting(self):
+        pass
+
+    def testInitialise(self):
+        pass
+
+
+class TestRepoStatusHandler(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testCheck(self):
+        pass
+
+
+class TestCommandLine(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testCheck(self):
+        pass
+
+    def testInit(self):
+        pass
+
+    def testPatch(self):
+        pass
+
+    def testMinor(self):
+        pass
+
+    def testMajor(self):
+        pass
